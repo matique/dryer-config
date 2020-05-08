1967ce0000a6a839c392cdcde1203a3b739daf38
matique <dittmar.krall@matique.de>
2020-05-08 13:07:44 +0200

    'v6.0.3'

--- lib/dryer-config/version.rb
+++ lib/dryer-config/version.rb
@@ -1,5 +1,6 @@
 module DryerConfig
-  VERSION = '6.0.2'     # 2020-02-25
+  VERSION = '6.0.3'     # 2020-05-08
+# VERSION = '6.0.2'     # 2020-02-25
 # VERSION = '6.0.0'     # 2019-10-07
 # VERSION = '5.2.0'
 end
