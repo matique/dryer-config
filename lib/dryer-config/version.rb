d8cdb79dfd5de45beeec2b8d09574735e8313800
dittmar <dittmar.krall@matique.de>
2021-12-18 14:41:44 +0100

    'v7.0.0'

--- lib/dryer-config/version.rb
+++ lib/dryer-config/version.rb
@@ -1,5 +1,6 @@
 module DryerConfig
-  VERSION = '6.1.4'     # 2021-06-27
+  VERSION = '7.0.0'     # 2021-12-18
+# VERSION = '6.1.4'     # 2021-06-27
 # VERSION = '6.0.3'     # 2020-05-08
 # VERSION = '6.0.2'     # 2020-02-25
 # VERSION = '6.0.0'     # 2019-10-07
